c1222814656b64e0340ef6805d4043c3b4241f12
Salamander3 <benjamin.sentirmay@patelhs.org>
2020-09-23 17:49:09 +0000

    Added the all the required code code

--- Main.java
+++ Main.java
@@ -1,5 +1,17 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    int myAge = 16;
+    int myBirthday = 1015;
+    int todayDate = 923;
+    if (myBirthday >= todayDate)
+    {
+      System.out.println("Your Birthday has not happen yet");
+      System.out.println("Your age is " + myAge);
+    }else{
+      myAge ++;
+      System.out.println("Your Brithday has happened");
+      System.out.println("Your age is " + myAge);
+    }
+    System.out.println("RUNS!");
   }
 }
